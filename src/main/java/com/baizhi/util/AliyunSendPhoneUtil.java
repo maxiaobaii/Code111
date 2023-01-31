8915f240c3ec34f879db8ef1d437049b1a7cb724
maxiaobai <61737472+maxiaobaii@users.noreply.github.com>
2023-01-31 17:17:42 +0800

    Update AliyunSendPhoneUtil.java

--- src/main/java/com/baizhi/util/AliyunSendPhoneUtil.java
+++ src/main/java/com/baizhi/util/AliyunSendPhoneUtil.java
@@ -27,8 +27,8 @@ public class AliyunSendPhoneUtil {
         final String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
         final String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
         //替换成你的AK
-        final String accessKeyId = "LTAI4G5UMgiJX68FJdio9jnG";//你的accessKeyId,参考本文档步骤2
-        final String accessKeySecret = "Lr2RmJvktEgy84j2tgmeBNwJHZX9Iz";//你的accessKeySecret，参考本文档步骤2
+        final String accessKeyId = "";//你的accessKeyId,参考本文档步骤2
+        final String accessKeySecret = "";//你的accessKeySecret，参考本文档步骤2
         //初始化ascClient,暂时不支持多region（请勿修改）
         IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId,
                 accessKeySecret);
